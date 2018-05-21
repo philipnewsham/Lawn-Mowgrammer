2e972e153679f106535c53ea92ea5c33ceda0313
PhilipN <philip.newsham@hotmail.co.uk>
2018-05-21 21:26:12 +0100

    change animation script

--- Assets/Scripts/AnimateIn.cs
+++ Assets/Scripts/AnimateIn.cs
@@ -15,20 +15,22 @@ public class AnimateIn : MonoBehaviour
         canvasGroup = GetComponent<CanvasGroup>();
         end = 0.0f;
         start = (Screen.width / 2.0f) + (rectTransform.sizeDelta.x / 2.0f);
-        StartCoroutine(Animation());
+        Vector2 startPosition = new Vector2(rectTransform.localPosition.x, rectTransform.localPosition.y - rectTransform.sizeDelta.y);
+        Vector2 endPosition = new Vector2(rectTransform.localPosition.x, rectTransform.localPosition.y);
+        StartCoroutine(Animation(startPosition, endPosition));
     }
 
-    IEnumerator Animation()
+    IEnumerator Animation(Vector2 start, Vector2 end)
     {
         float lerpTime = 0.0f;
         while(lerpTime < 1.0f)
         {
-            rectTransform.localPosition = new Vector2(Mathf.Lerp(start, end, lerpTime), 0.0f);
+            rectTransform.localPosition = Vector2.Lerp(start, end, lerpTime);
             canvasGroup.alpha = lerpTime;
             lerpTime += Time.deltaTime * 2.0f;
             yield return null;
         }
-        rectTransform.localPosition = new Vector2(end, 0.0f);
+        rectTransform.localPosition = end;
         canvasGroup.alpha = 1.0f;
     }
 }

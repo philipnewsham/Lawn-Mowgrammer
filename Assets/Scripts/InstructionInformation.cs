68c1f29b791858539c07cea99388a0b13e9a99e4
PhilipN <philip.newsham@hotmail.co.uk>
2018-06-03 11:53:59 +0100

    use drag instruction system

---
+++ Assets/Scripts/InstructionInformation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionInformation : MonoBehaviour
+{
+    public Instruction instruction;
+    public InputField jumpToInputField;
+    public InputField checkLetterInputField;
+    public Dropdown checkOperaterDropdown;
+    public InputField checkCountInputField;
+
+    public void SetJumpTo()
+    {
+        instruction.jumpTo = int.Parse(jumpToInputField.text);
+    }
+
+    public void SetCheckLetter()
+    {
+        instruction.checkLetter = int.Parse(checkLetterInputField.text);
+    }
+
+    public void SetCheckOperator()
+    {
+        instruction.checkOperator = (Instruction.Operator)checkOperaterDropdown.value;
+    }
+
+    public void SetCheckCount()
+    {
+        instruction.checkCount = int.Parse(checkCountInputField.text);
+    }
+}
